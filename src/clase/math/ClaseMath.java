2f485c6161fd707c7bf41c50a0879b4c42c12cd0
Eduardo <eduarmudaw@gmail.com>
2019-10-09 20:14:19 +0200

    Prueba prueba prueba

---
+++ src/clase/math/ClaseMath.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clase.math;
+
+/**
+ *
+ * @author Deftkain
+ */
+public class ClaseMath {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    
+        //double raiz=Math.sqrt(9);
+        double base=5;
+        double exponente=3;
+        System.out.println (base);
+       
+        int resultado=(int)Math.pow(base, exponente);
+        
+        
+        System.out.println("El resultado de " + base + " elevado a " + exponente + "es: " + resultado);
+              
+            
+        
+    }
+    
+}
